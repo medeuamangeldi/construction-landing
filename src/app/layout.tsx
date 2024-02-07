8f6e97a13d3b221b405239f65d8b4f2ffbdaebd4
Medeu Amangeldi <medcod49@gmail.com>
2024-02-07 09:53:39 -0600

    added logo, case names and updated images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anuar Construction",
+  title: "Bright Alim Corporation",
   description: "Quality, price and comfort",
 };
 
