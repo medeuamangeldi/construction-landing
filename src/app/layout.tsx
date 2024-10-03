cdc72fce8a214c57eb07a843b838b3069465e8c3
Medeu Amangeldi <medcod49@gmail.com>
2024-10-03 11:22:32 -0500

    feat: updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,42 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bright Alim Corporation",
-  description: "Quality, price and comfort",
+  description:
+    "Explore cutting-edge solutions for Bitcoin mining with our expert services in ASIC repair, microcircuit repair, and hashboard repair. Discover innovative cooling solutions, including adiabatic cooling and filtration solutions, designed to prevent Bitcoin mining overheating. Our metal building construction, featuring steel framing construction and adiabatic panels, ensures efficient operation by effectively separating the cold zone from the hot one. We specialize in daily operation and maintenance for top brands like Bitmain, Whatsminer, and Avalon miner. Optimize your mining setup today with our comprehensive services, including spreading gravel and industrial welding for enhanced durability.",
+  keywords: [
+    "Bitcoin mining",
+    "Asic repair",
+    "Adiabatic cooling",
+    "Microcircuit repair",
+    "Cooling solutions",
+    "Metal Building Construction",
+    "Hashboard repair",
+    "Filtration solutions",
+    "Bitmain",
+    "Whatsminer",
+    "Avalon miner",
+    "Construction",
+    "Adiabatic panels",
+    "Bitcoin mining overheating",
+    "Steel framing construction",
+    "Separate the cold zone from the hot one",
+    "Spread gravel",
+    "Industrial Welding",
+    "Daily operation and maintenance",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
